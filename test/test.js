21247ffb0f37196e738cbc82b2d33ebf75ed2a00
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2016-04-04 11:41:15 +0530

    Fix:JS LInt issue - Code Style

--- test/test.js
+++ test/test.js
@@ -2,37 +2,36 @@ var http = require('http');
 var request = require('request');
 var assert = require('assert');
 describe('loading express', function() {
-    beforeEach(function() {
-        // Start Proxy Server and Other Dependent Server
-        var proxyServer = require('../app');
+	beforeEach(function() {
+		// Start Proxy Server and Other Dependent Server
+		var proxyServer = require('../app');
 
-        var server1 = require('../server1');
-        var server2 = require('../server2');
-        var server3 = require('../server3');
-        http.createServer(server1);
-        http.createServer(server2);
-        http.createServer(server3);
-        http.createServer(proxyServer);
-    });
-    it('Proxy to Server1', function testPath(done) {
-        request('http://localhost:3000/api/v1/carton', function(err, res, body) {
-            assert.equal("Hello world From TX-Man, Here Is the Carton v1", res.body)
-            done()
-        })
-    });
+		var server1 = require('../server1');
+		var server2 = require('../server2');
+		var server3 = require('../server3');
+		http.createServer(server1);
+		http.createServer(server2);
+		http.createServer(server3);
+		http.createServer(proxyServer);
+	});
+	it('Proxy to Server1', function testPath(done) {
+		request('http://localhost:3000/api/v1/carton', function(err, res /*, body*/) {
+			assert.equal('Hello world From TX-Man, Here Is the Carton v1', res.body);
+			done();
+		});
+	});
 
-    it('Proxy to Server2', function testPath(done) {
-        request('http://localhost:3000/api/v2/carton', function(err, res, body) {
-            assert.equal("Hello world From ImageServer, Here Is the Carton V2", res.body)
-            done()
-        })
-    });
+	it('Proxy to Server2', function testPath(done) {
+		request('http://localhost:3000/api/v2/carton', function(err, res /*, body*/) {
+			assert.equal('Hello world From ImageServer, Here Is the Carton V2', res.body);
+			done();
+		});
+	});
 
-    it('Proxy to Server3', function testPath(done) {
-        request('http://localhost:3000/api/v3/carton', function(err, res, body) {
-            assert.equal("Hello world From pimhunt, Here Is the Carton v3", res.body)
-            done()
-        })
-    });
-
+	it('Proxy to Server3', function testPath(done) {
+		request('http://localhost:3000/api/v3/carton', function(err, res /*, body*/) {
+			assert.equal('Hello world From pimhunt, Here Is the Carton v3', res.body);
+			done();
+		});
+	});
 });
